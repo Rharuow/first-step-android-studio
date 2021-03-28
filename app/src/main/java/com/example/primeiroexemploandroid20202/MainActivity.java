a3d8b414f248f2ac40c7e710537f2d357c47be1f
Harysson Soares <haryssonsoares@gmail.com>
2021-03-28 17:36:52 -0300

    lifecicle methods

--- app/src/main/java/com/example/primeiroexemploandroid20202/MainActivity.java
+++ app/src/main/java/com/example/primeiroexemploandroid20202/MainActivity.java
@@ -10,11 +10,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle estado) {
         super.onCreate(estado);
         System.out.println("onCreate()");
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
     }
 
     protected void onStart() {
         super.onStart();
         System.out.println("onStart()");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        System.out.println("onResume()");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        System.out.println("onPause()");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        System.out.println("onStop()");
+    }
+
+    //Ao rotacionar ele também é chamado;
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("onDestroy()");
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("onRestart()");
     }
 }
