c5ec1aaaa1fd7662e740e166240361c26090f642
Harysson Soares <haryssonsoares@gmail.com>
2021-03-28 12:17:44 -0300

    onCreate and onStart

--- app/src/main/java/com/example/primeiroexemploandroid20202/MainActivity.java
+++ app/src/main/java/com/example/primeiroexemploandroid20202/MainActivity.java
@@ -7,8 +7,14 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle estado) {
+        super.onCreate(estado);
+        System.out.println("onCreate()");
+        //setContentView(R.layout.activity_main);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        System.out.println("onStart()");
     }
 }
